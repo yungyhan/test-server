52ae22341831ecaf10d92a7210f0f674e51bdd8f
Han Lim <yungyhan@gmail.com>
2023-02-27 21:56:36 +0800

    add google apis

--- dist/server.js
+++ dist/server.js
@@ -1,5 +1,5 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
 // Schema definition
 export const typeDefs = `
   #graphql
@@ -7,26 +7,40 @@ export const typeDefs = `
     hello(name: String): String!
   }
 `;
+export const typeDefs2 = `
+  #graphql
+  type Query {
+    helloxxx(name: String): String!
+  }
+`;
 // Resolver map
 export const resolvers = {
     Query: {
-        hello: (_, { name }) => `Hello ${name}!`,
+        hello: (_, { name }) => `Helloxxxx ${name}!`,
+    },
+};
+// Resolver map
+export const resolvers2 = {
+    Query: {
+        helloxxx: (_, { name }) => `123 ${name}!`,
     },
 };
 // This function will create a new server Apollo Server instance
 export const createApolloServer = async (listenOptions = { port: 4000 }) => {
     const server = new ApolloServer({
-        typeDefs,
-        resolvers,
+        typeDefs: [typeDefs, typeDefs2],
+        resolvers: [resolvers, resolvers2],
     });
-    const { url } = await startStandaloneServer(server, { listen: listenOptions });
+    const { url } = await startStandaloneServer(server, {
+        listen: listenOptions,
+    });
     // return the server instance and the url the server is listening on
     return { server, url };
 };
 // For simplicity we create our server in this file,
 // but in a real app you'd export `createApolloServer` into
 // another file and call it elsewhere.
-if (process.env.NODE_ENV !== 'test') {
+if (process.env.NODE_ENV !== "test") {
     const { url } = await createApolloServer();
     console.log(`🚀 Query endpoint ready at ${url}`);
 }
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import type { ListenOptions } from 'net';
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import type { ListenOptions } from "net";
 
 // Schema definition
 export const typeDefs = `
@@ -10,21 +10,39 @@ export const typeDefs = `
   }
 `;
 
+export const typeDefs2 = `
+  #graphql
+  type Query {
+    helloxxx(name: String): String!
+  }
+`;
+
 // Resolver map
 export const resolvers = {
   Query: {
-    hello: (_, { name }) => `Hello ${name}!`,
+    hello: (_, { name }) => `Helloxxxx ${name}!`,
+  },
+};
+
+// Resolver map
+export const resolvers2 = {
+  Query: {
+    helloxxx: (_, { name }) => `123 ${name}!`,
   },
 };
 
 // This function will create a new server Apollo Server instance
-export const createApolloServer = async (listenOptions: ListenOptions = { port: 4000 }) => {
+export const createApolloServer = async (
+  listenOptions: ListenOptions = { port: 4000 }
+) => {
   const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+    typeDefs: [typeDefs, typeDefs2],
+    resolvers: [resolvers, resolvers2],
   });
 
-  const { url } = await startStandaloneServer(server, { listen: listenOptions });
+  const { url } = await startStandaloneServer(server, {
+    listen: listenOptions,
+  });
 
   // return the server instance and the url the server is listening on
   return { server, url };
@@ -33,7 +51,7 @@ export const createApolloServer = async (listenOptions: ListenOptions = { port: 
 // For simplicity we create our server in this file,
 // but in a real app you'd export `createApolloServer` into
 // another file and call it elsewhere.
-if (process.env.NODE_ENV !== 'test') {
+if (process.env.NODE_ENV !== "test") {
   const { url } = await createApolloServer();
   console.log(`🚀 Query endpoint ready at ${url}`);
 }
